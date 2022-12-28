4cc53d84193a11f1cd745c31f7d1c8002ae85288
Ehsan Amiri <ehsan.amiri.code@gmail.com>
2022-12-28 20:07:57 +0330

    Designing the Canvas 0.1

--- GameOfLife/GameOfLife.go
+++ GameOfLife/GameOfLife.go
@@ -1,6 +1,10 @@
 package main
 
-var height, width = 50
+var width, height = 0
+fmt.Println("The Width of your canvas is ")
+fmt.Scanln(&width)
+fmt.Println("The Height of your canvas is ")
+fmt.Scanln(&height)
 
 func main() {
 
