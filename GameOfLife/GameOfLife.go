886f6759968ca04844c0b33c063c5997add0e459
Ehsan Amiri <ehsan.amiri.code@gmail.com>
2022-12-29 21:54:56 +0330

    feat: add the field in which Game Of Life is being played on

--- GameOfLife/GameOfLife.go
+++ GameOfLife/GameOfLife.go
@@ -1,20 +1,34 @@
 package main
- import (
+
+import (
 	"fmt"
-	"math/rand"
-	"time")
+	//"math/rand"
+	//"time"
+)
 
-var width, height int = 0, 0
-fmt.Println("The Width of your canvas is ")
-fmt.Scanln(&width)
-fmt.Println("The Height of your canvas is ")
-fmt.Scanln(&height)
+var (
+	width  = 20
+	height = 20
+)
 
-func main() {
-	for x = 0; x < width; x++ {
-		column = []
-		for y = 0; y < height; y++{
-			if rand
+func field() {
+	s := make([][]string, width, height)
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			fmt.Println(s)
 		}
 	}
+	fmt.Println(s)
+}
+func initialGen() {
+
+}
+
+func main() {
+	fmt.Println("The Width of your canvas is ", width)
+	//fmt.Scanln(&width)
+	fmt.Println("The Height of your canvas is ", height)
+	//fmt.Scanln(&height)
+
+	field()
 }
