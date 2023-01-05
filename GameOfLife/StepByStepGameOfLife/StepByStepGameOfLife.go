02c493fc7365058a716bb26f49c8274396906710
Ehsan Amiri <ehsan.amiri.code@gmail.com>
2023-01-05 23:22:04 +0330

    docs: add comments to explain different parts of the code

--- GameOfLife/StepByStepGameOfLife/StepByStepGameOfLife.go
+++ GameOfLife/StepByStepGameOfLife/StepByStepGameOfLife.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 )
 
-type Field struct {
-	height int
-	width  int
+type Field struct { // defines a Field type
+	height int //in which we declare
+	width  int // size of the grid
 }
 
-func (field Field) drawField() {
-	s := make([][]bool, field.height)
-	for i := 0; i < field.height; i++ {
-		s[i] = make([]bool, field.width)
-		for j := 0; j < field.width; j++ {
-			if j%2 == 0 {
+func (field Field) drawField() { // drawField method of the Field structs
+	s := make([][]bool, field.height)   // generates the grid through a 2dinmetional slice
+	for i := 0; i < field.height; i++ { // we nest a slice inside another slice via iterating
+		s[i] = make([]bool, field.width)   // i index refers to the height of the grid
+		for j := 0; j < field.width; j++ { // j index refers to the width of the grid
+			if j%2 == 0 { // set the grid values based on modulus of the index
 				s[i][j] = true
 			} else {
 				s[i][j] = false
@@ -25,6 +25,6 @@ func (field Field) drawField() {
 	fmt.Println(s)
 }
 func main() {
-	field := Field{20, 10}
+	field := Field{20, 10} // set the size of the grid via parameters
 	Field.drawField(field)
 }
