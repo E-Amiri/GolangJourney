f208c891266b4a10b128acb94e271cd739f3d7f9
Ehsan Amiri <ehsan.amiri.code@gmail.com>
2023-01-05 22:52:55 +0330

    feat: drawField method now generates a 2d boolean grid ased on %2 modulus

--- GameOfLife/StepByStepGameOfLife/StepByStepGameOfLife.go
+++ GameOfLife/StepByStepGameOfLife/StepByStepGameOfLife.go
@@ -11,21 +11,30 @@ type Field struct {
 }
 
 func (field Field) drawField() {
-	s := make([][]int, field.height)
+	s := make([][]bool, field.height)
 	for i := 0; i < field.height; i++ {
-		s[i] = make([]int, field.width)
+		s[i] = make([]bool, field.width)
+		for j := 0; j < field.width; j++ {
+			if j%2 == 0 {
+				s[i][j] = true
+			} else {
+				s[i][j] = false
+			}
+
+		}
 	}
 	fmt.Println(s)
 }
 
 /*
-	func (field Field) setValue() bool{
-		rand.Seed(time.Now().UnixNano())
-		if rand.Int()
-
-}
+	func (field Field) setValue(x, y int, b bool) [][]bool {
+		field.mainSlice[x][y] = b
+		return field.mainSlice
+	}
 */
 func main() {
+	//var f Field
+	//var s [][]bool = Field.setValue(f, 20, 10, true)
 	field := Field{20, 10}
 	Field.drawField(field)
 	//field = Field{20, 30}
