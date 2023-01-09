1d1b3875acdfa22abb0c05944b9221696c78d74e
Ehsan Amiri <ehsan.amiri.code@gmail.com>
2023-01-09 22:45:52 +0330

    fix: showing number of columns via terminal without brackets

--- GameOfLife/StepByStepGameOfLife/StepByStepGameOfLife.go
+++ GameOfLife/StepByStepGameOfLife/StepByStepGameOfLife.go
@@ -7,24 +7,31 @@ import (
 func main() {
 	var t []string
 	var f []string
+	height := 0
+	width := 0
 
-	height := 20
-	width := 10
+	fmt.Println("Please Enter the Height of Your Grid:")
+	fmt.Scan(&height)
+	fmt.Println("Please Enter the Width of Your Grid:")
+	fmt.Scan(&width)
+
 	s := make([][]string, height)
 	for i := 0; i < height; i++ {
 		s[i] = make([]string, width)
+		fmt.Printf("\n")
 		for j := 0; j < width; j++ {
 			if j%2 == 0 {
 				s[i][j] = "*"
 				t = append(t, s[i][j])
+				fmt.Printf("%v", s[i][j])
 			} else {
 				s[i][j] = "-"
 				f = append(f, s[i][j])
+				fmt.Printf("%v", s[i][j])
 			}
 
 		}
 	}
-	fmt.Println(s, "\n")
-	fmt.Println("Total Alive cells =", len(t))
+	fmt.Println("\nTotal Alive cells =", len(t))
 	fmt.Println("Total Dead cells =", len(f))
 }
