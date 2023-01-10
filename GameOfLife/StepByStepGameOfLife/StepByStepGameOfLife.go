c4e88c5232c81be6e5f20c04220a314e15394383
Ehsan Amiri <ehsan.amiri.code@gmail.com>
2023-01-10 23:11:48 +0330

    feat: add random Dead or Alive values and Counitous run

--- GameOfLife/StepByStepGameOfLife/StepByStepGameOfLife.go
+++ GameOfLife/StepByStepGameOfLife/StepByStepGameOfLife.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
@@ -9,29 +11,33 @@ func main() {
 	var f []string
 	height := 20
 	width := 20
+	rand.Seed(time.Now().UnixNano())
 
 	fmt.Println("Please Enter the Height of Your Grid:")
 	fmt.Scan(&height)
 	fmt.Println("Please Enter the Width of Your Grid:")
 	fmt.Scan(&width)
 
-	s := make([][]string, height)
-	for i := 0; i < height; i++ {
-		s[i] = make([]string, width)
-		fmt.Printf("\n")
-		for j := 0; j < width; j++ {
-			if j%2 == 0 {
-				s[i][j] = "*"
-				t = append(t, s[i][j])
-				fmt.Printf("%v", s[i][j])
-			} else {
-				s[i][j] = "-"
-				f = append(f, s[i][j])
-				fmt.Printf("%v", s[i][j])
+	for {
+		s := make([][]string, height)
+		for i := 0; i < height; i++ {
+			s[i] = make([]string, width)
+			fmt.Printf("\n")
+			for j := 0; j < width; j++ {
+				if rand.Intn(10)%2 == 0 {
+					s[i][j] = "*"
+					t = append(t, "*")
+					fmt.Printf("%v", s[i][j])
+				} else {
+					s[i][j] = "-"
+					f = append(f, "*")
+					fmt.Printf("%v", s[i][j])
+				}
 			}
-
 		}
+
+		fmt.Println("\nTotal Alive cells =", len(t))
+		fmt.Println("Total Dead cells =", len(f))
+		time.Sleep(1 * time.Second)
 	}
-	fmt.Println("\nTotal Alive cells =", len(t))
-	fmt.Println("Total Dead cells =", len(f))
 }
